9e37c98e402af2119dea8ee029b5369291f20e2c
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-10-01 04:15:39 +0530

    done

--- socialProjectApp/forms.py
+++ socialProjectApp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
 class ProfileSearchForm(forms.Form):
-  text_input = forms.CharField(
-    label='',
-    widget=forms.TextInput(attrs={'placeholder': 'Enter username of Instagram', 'id': 'username', 'class': 'centered-input'}),
-    required=True
+    text_input = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter username of Instagram',
+            'id': 'username',
+            'class': 'centered-input',
+            'style': 'border: none; outline: none;'  # Remove border and outline
+        }),
+        required=True
     )
-
