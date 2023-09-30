939e3f76b006e6fe4fa6dd2c6bc89e22c67ecc98
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-10-01 04:25:59 +0530

    working

--- socialProjectApp/views.py
+++ socialProjectApp/views.py
@@ -23,37 +23,32 @@ clf = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
 clf.fit(X, y)
 
 # Function to predict fake accounts
+
 def predict_fake_account(request):
-    
+    prediction = None
+    followers_count = None
+    following_count = None
+
     if request.method == 'POST':
         form = ProfileSearchForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['text_input']
-            # username='dsd'
             try:
-                # Initialize Instaloader
                 ig = instaloader.Instaloader()
-                
-                # Fetch the details of the provided username using Instaloader
                 profile = instaloader.Profile.from_username(ig.context, username)
 
-                # Extract relevant features
                 posts_on_website = profile.mediacount
                 followers_count = profile.followers
                 following_count = profile.followees
 
-                # Make prediction based on extracted features
-                user_input = [
-                [posts_on_website, followers_count]
-                ]
+                user_input = [[posts_on_website, followers_count]]
 
                 prediction = clf.predict(user_input)[0]
 
-                return render(request, 'result.html', {'prediction': prediction, 'followers': followers_count, 'following': following_count})
             except Exception as e:
                 return HttpResponse(f'An error occurred: {str(e)}')
-    
+
     else:
         form = ProfileSearchForm()
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {'form': form, 'prediction': prediction, 'followers': followers_count, 'following': following_count})
